6f5ead7c085d8cd07e7989c90d5d5dad363d8022
Cricka <saerevall@gmail.com>
2016-04-29 15:33:37 +0200

    retrieve IP from firebase

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ function setupSocketClient(){
 
 		ws.onmessage = function(evt){
 			console.log(evt.data);
-			$("#depth-range-lower").slider('values',0,50);
+			$("#depth-range-lower").slider('values',50);
 		};
 
 		ws.onopen = function(evt){
