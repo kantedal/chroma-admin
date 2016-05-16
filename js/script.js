abcbedda3fa9782af98d2ecba43232d923bd7ac1
Karin <karin_lr@hotmail.com>
2016-05-16 16:26:30 +0200

    anslutningsindikator

--- js/script.js
+++ js/script.js
@@ -2,15 +2,15 @@ var ws;
 var isConnected = false;
 
 
-var maskingColorRed = 0;
-var maskingColorGreen = 1;
-var maskingColorBlue = 2;
+// var maskingColorRed = 0;
+// var maskingColorGreen = 1;
+// var maskingColorBlue = 2;
 var innerRadius = 3;
 var outerRadius = 4;
-var maskingColorDistance = 5;
+// var maskingColorDistance = 5;
 var maskingColorAuto = 6;
 var innerDepthRadius = 7;
-var outerDepthRadius = 8;	
+// var outerDepthRadius = 8;	
 
 var ref = new Firebase("https://kromakey.firebaseio.com/data");
 //Setup web socket client for communication 
@@ -32,6 +32,7 @@ function setupSocketClient(){
 
 		ws.onopen = function(evt){
 			isConnected = true;
+			$( "div.connection-container" ).text( "Ansluten!" );
 		}
 	  
 	}, function (errorObject) {
@@ -52,9 +53,9 @@ function sendMessage(attr, value){
 //Add UI elements
 function setupUIElements() {
 
-	$("#color-tolerance").on("change", function(){
-	    sendMessage(maskingColorDistance, $(this).val());
-	});
+	// $("#color-tolerance").on("change", function(){
+	//     sendMessage(maskingColorDistance, $(this).val());
+	// });
 
 	$("#inner-radius").on("change", function(){
 		//check so inner radius cannot be larger than outer radius
@@ -73,67 +74,67 @@ function setupUIElements() {
 
 	$("#inner-depth-radius").on("change", function(){
 		//check so inner radius cannot be larger than outer radius
-		if ($(this).val() > $("#outer-depth-radius").val()){
-			$(this).val($("#outer-depth-radius").val());
-		}
+		// if ($(this).val() > $("#outer-depth-radius").val()){
+		// 	$(this).val($("#outer-depth-radius").val());
+		// }
 	    sendMessage(innerDepthRadius, $(this).val());
 	});
-	$("#outer-depth-radius").on("change", function(){
-		//check so outer radius cant be smaller than inner radius
-		if ($(this).val() < $("#inner-depth-radius").val()){
-			$(this).val($("#inner-depth-radius").val());
-		}
-	    sendMessage(outerDepthRadius, $(this).val());
-	});
+	// $("#outer-depth-radius").on("change", function(){
+	// 	//check so outer radius cant be smaller than inner radius
+	// 	if ($(this).val() < $("#inner-depth-radius").val()){
+	// 		$(this).val($("#inner-depth-radius").val());
+	// 	}
+	//     sendMessage(outerDepthRadius, $(this).val());
+	// });
 
-	$("#masking-color-red").on("change", function(){
-	    sendMessage(maskingColorRed, $(this).val());
-	    $("#mixed-color").css('background', "rgb("+
-    		$("#masking-color-red").val()+","+
-    		$("#masking-color-green").val()+","+
-    		$("#masking-color-blue").val()+")");
-	});
+	// $("#masking-color-red").on("change", function(){
+	//     sendMessage(maskingColorRed, $(this).val());
+	//     $("#mixed-color").css('background', "rgb("+
+ //    		$("#masking-color-red").val()+","+
+ //    		$("#masking-color-green").val()+","+
+ //    		$("#masking-color-blue").val()+")");
+	// });
 
-	$("#masking-color-green").on("change", function(){
-	    sendMessage(maskingColorGreen, $(this).val());
-	    $("#mixed-color").css('background', "rgb("+
-    		$("#masking-color-red").val()+","+
-    		$("#masking-color-green").val()+","+
-    		$("#masking-color-blue").val()+")");
-	});
+	// $("#masking-color-green").on("change", function(){
+	//     sendMessage(maskingColorGreen, $(this).val());
+	//     $("#mixed-color").css('background', "rgb("+
+ //    		$("#masking-color-red").val()+","+
+ //    		$("#masking-color-green").val()+","+
+ //    		$("#masking-color-blue").val()+")");
+	// });
 
-	$("#masking-color-blue").on("change", function(){
-	    sendMessage(maskingColorBlue, $(this).val());
-	    $("#mixed-color").css('background', "rgb("+
-    		$("#masking-color-red").val()+","+
-    		$("#masking-color-green").val()+","+
-    		$("#masking-color-blue").val()+")");
-	});
+	// $("#masking-color-blue").on("change", function(){
+	//     sendMessage(maskingColorBlue, $(this).val());
+	//     $("#mixed-color").css('background', "rgb("+
+ //    		$("#masking-color-red").val()+","+
+ //    		$("#masking-color-green").val()+","+
+ //    		$("#masking-color-blue").val()+")");
+	// });
 }
 
 function changeUIElements(index, val) {
 
    switch(index){
-   	case maskingColorRed:
-   		$( "#masking-color-red" ).val( val );
-   		$( "#masking-color-red" ).slider("refresh");
-   	break;
-   	case maskingColorGreen:
-   		$( "#masking-color-green" ).val( val );
-   		$( "#masking-color-green" ).slider("refresh");
-   	break;
-   	case maskingColorBlue:
-   		$( "#masking-color-blue" ).val( val );
-   		$( "#masking-color-blue" ).slider( "refresh");
-   	break;
+   	// case maskingColorRed:
+   	// 	$( "#masking-color-red" ).val( val );
+   	// 	$( "#masking-color-red" ).slider("refresh");
+   	// break;
+   	// case maskingColorGreen:
+   	// 	$( "#masking-color-green" ).val( val );
+   	// 	$( "#masking-color-green" ).slider("refresh");
+   	// break;
+   	// case maskingColorBlue:
+   	// 	$( "#masking-color-blue" ).val( val );
+   	// 	$( "#masking-color-blue" ).slider( "refresh");
+   	// break;
    	case innerDepthRadius:
    		$( "#inner-depth-radius" ).val( val );
    		$( "#inner-depth-radius" ).slider( "refresh");
    	break;
-   	case outerDepthRadius:
-   		$( "#outer-depth-radius" ).val( val );
-   		$( "#outer-depth-radius" ).slider("refresh");
-   	break;
+   	// case outerDepthRadius:
+   	// 	$( "#outer-depth-radius" ).val( val );
+   	// 	$( "#outer-depth-radius" ).slider("refresh");
+   	// break;
    	case innerRadius:
    		$( "#inner-radius" ).val( val );
    		$( "#inner-radius" ).slider( "refresh");
@@ -142,24 +143,24 @@ function changeUIElements(index, val) {
    		$( "#outer-radius" ).val( val );
 		$( "#outer-radius" ).slider("refresh");
    	break;
-   	case maskingColorDistance:
-   		$( "#color-tolerance" ).val( val );
-		$( "#color-tolerance" ).slider("refresh");
-   	break;
+  //  	case maskingColorDistance:
+  //  		$( "#color-tolerance" ).val( val );
+		// $( "#color-tolerance" ).slider("refresh");
+  //  	break;
    }
 }
 
 
 function fabricsSettings(){
 
-	sendMessage(0, 41);
-	sendMessage(1, 210);
-	sendMessage(2, 93);
+	//sendMessage(0, 41);
+	//sendMessage(1, 210);
+	//sendMessage(2, 93);
 	sendMessage(3, 0.3*10000);
 	sendMessage(4, 0.33*10000);
-	sendMessage(5, 24);
+	// sendMessage(5, 24);
 	sendMessage(7, 0.3*10000);
-	sendMessage(8, 0.33*10000);
+	//sendMessage(8, 0.33*10000);
 }
 
 window.onload = function(){
